b76050fec687a623a65f28b126cc3c6b0e75cf49
chickenlittle5 <132306572+chickenlittle5@users.noreply.github.com>
2026-05-05 20:43:53 -0700

    finished with explore part1 working on speech synthesis

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Lab 5 - Starter
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 Ethan Tran
+
+https://chickenlittle5.github.io/Lab5_Starter/
+
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,50 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const face = document.querySelector('#explore img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+
+  let voices = [];
+
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  loadVoices();
+
+  speechSynthesis.addEventListener('voiceschanged', loadVoices);
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textArea.value);
+    const selectedVoice = voices[voiceSelect.value];
+
+    if (selectedVoice) {
+      utterance.voice = selectedVoice;
+    }
+
+    utterance.addEventListener('start', () => {
+      face.src = 'assets/images/smiling-open.png';
+      face.alt = 'Smiling face speaking';
+    });
+
+    utterance.addEventListener('end', () => {
+      face.src = 'assets/images/smiling.png';
+      face.alt = 'Smiling face';
+    });
+
+    speechSynthesis.speak(utterance);
+  });
 }
